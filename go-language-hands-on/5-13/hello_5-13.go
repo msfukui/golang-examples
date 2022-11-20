8d4921c94708f5402cc28bfc53031f180130ebb0
msfukui <msfukui@gmail.com>
2022-11-20 19:05:03 +0900

    finished chapter 5-2

---
+++ go-language-hands-on/5-13/hello_5-13.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+// Mydata is json structure.
+type Mydata struct {
+	Name string
+	Mail string
+	Tel  string
+}
+
+// Str get string value.
+func (m *Mydata) Str() string {
+	return "<\"" + m.Name + "\" " + m.Mail + ", " + m.Tel + ">"
+}
+
+func main() {
+	p := "https://tuyano-dummy-data.firebaseio.com/mydata.json"
+	re, er := http.Get(p)
+	if er != nil {
+		panic(er)
+	}
+	defer re.Body.Close()
+
+	s, er := io.ReadAll(re.Body)
+	if er != nil {
+		panic(er)
+	}
+
+	var itms []Mydata
+	er = json.Unmarshal(s, &itms)
+	if er != nil {
+		panic(er)
+	}
+
+	for i, im := range itms {
+		println(i, im.Str())
+	}
+}
