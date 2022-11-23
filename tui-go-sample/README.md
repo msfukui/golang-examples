dcdc966933780f06f6690df07bb3c6358a2d03dc
msfukui <msfukui@gmail.com>
2022-11-23 11:46:14 +0900

    Add code and README for tui-go-sample

---
+++ tui-go-sample/README.md
@@ -0,0 +1,5 @@
+# tui-go-sample
+
+A sample codes in use `tui-go`.
+
+https://github.com/marcusolsson/tui-go
---
+++ tui-go-sample/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/marcusolsson/tui-go"
+
+func main() {
+	box := tui.NewVBox(
+		tui.NewLabel("こんにちは、せかい"),
+	)
+	ui, err := tui.New(box)
+	if err != nil {
+		panic(err)
+	}
+	ui.SetKeybinding("Esc", func() { ui.Quit() })
+	if err := ui.Run(); err != nil {
+		panic(err)
+	}
+}
