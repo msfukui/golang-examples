2e88fa24d812b719cbc647d8c1e91cff284996b1
msfukui <msfukui@gmail.com>
2022-11-27 10:20:02 +0900

    finished chapter 6-1

--- go-language-hands-on/go-web/web.go
+++ go-language-hands-on/go-web/web.go
@@ -3,7 +3,15 @@ package main
 import "net/http"
 
 func main() {
-	if err := http.ListenAndServe("", http.FileServer(http.Dir("."))); err != nil {
+	msg := `<html><body>
+					<h1>Hello</h1>
+					<p>This is Go-server!</p>
+					</body></html>`
+	hh := func(w http.ResponseWriter, rq *http.Request) {
+		w.Write([]byte(msg))
+	}
+	http.HandleFunc("/hello", hh)
+	if err := http.ListenAndServe("", nil); err != nil {
 		panic(err)
 	}
 }
