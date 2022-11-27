b8da363082c9d9834f696a8a4f5fee89163d1f59
msfukui <msfukui@gmail.com>
2022-11-27 09:48:09 +0900

    Fix go-web/web.go, index.html, list 6-2, 6-3

--- go-language-hands-on/go-web/web.go
+++ go-language-hands-on/go-web/web.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 func main() {
-	if err := http.ListenAndServe("", http.NotFoundHandler()); err != nil {
+	if err := http.ListenAndServe("", http.FileServer(http.Dir("."))); err != nil {
 		panic(err)
 	}
 }
